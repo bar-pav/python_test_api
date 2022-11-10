8f3a8b01b2c232317820004e4bd1af9288c6d77d
Pavel <none>
2022-11-10 16:58:50 +0300

    Add function to create operation and update balance.

--- cost_accounting_mng/api/views.py
+++ cost_accounting_mng/api/views.py
@@ -5,6 +5,7 @@ from rest_framework.generics import (ListCreateAPIView, RetrieveUpdateDestroyAPI
 from rest_framework.permissions import IsAuthenticated, IsAdminUser, IsAuthenticatedOrReadOnly, AllowAny
 from django.contrib.auth.models import User
 from .serializers import UserProfileSerializer, OperationsSerializer
+from rest_framework.response import Response
 
 from .models import Operations, Balance
 
@@ -39,16 +40,28 @@ class UserOperationsListCreateView(ListCreateAPIView):
         # return Operations.objects.filter()
         return Operations.objects.filter(user=self.request.user)
 
+    def create(self, request):
+        serializer = self.get_serializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        res = self.perform_create(serializer)
+        headers = self.get_success_headers(serializer.data)
+        if res:
+            return Response(serializer.data, headers=headers)
+        else:
+            return Response({"message": "Insuffitient funds for operation."})
+
     def perform_create(self, serializer):
-        # pass
         balance = Balance.objects.filter(user=self.request.user).first()
         if not balance:
             balance = Balance.objects.create(user=self.request.user, balance=Decimal(self.request.data['amount']))
         else:
+            if Decimal(self.request.data['amount']) < 0 and balance.balance < abs(Decimal(self.request.data['amount'])):
+                return 
             balance.balance += Decimal(self.request.data['amount'])
             balance.save()
         serializer.save(user=self.request.user,
                         amount=self.request.data['amount'],
                         category=self.request.data['category'],
                         organization=self.request.data['organization'])
+        return 1
 
