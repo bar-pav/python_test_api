2e11dad7fce2da4436a40dd3a202386fe2d47098
Pavel <none>
2022-11-15 16:32:24 +0300

    modify useroperationslistcreateview to inherited from apiview

--- cost_accounting_mng/api/views.py
+++ cost_accounting_mng/api/views.py
@@ -2,6 +2,8 @@ from decimal import Decimal
 
 from django.shortcuts import render
 from rest_framework.generics import (ListCreateAPIView, RetrieveUpdateDestroyAPIView,)
+from rest_framework.views import APIView
+from rest_framework import status
 from rest_framework.permissions import IsAuthenticated, IsAdminUser, IsAuthenticatedOrReadOnly, AllowAny
 from django.contrib.auth.models import User
 from .serializers import UserProfileSerializer, OperationsSerializer, BalanceSerializer
@@ -31,58 +33,32 @@ class UserProfileListCreateView(ListCreateAPIView):
 #     serializer_class = UserProfileSerializer
 
 
-class UserOperationsListCreateView(ListCreateAPIView):
+class UserOperationsListCreateView(APIView):
     # queryset = Operations.objects.all()
     serializer_class = OperationsSerializer
     permission_classes = (IsAuthenticated,)
     balance_serializer = BalanceSerializer
 
     def get_queryset(self):
-        # return Operations.objects.filter()
         return Operations.objects.filter(user=self.request.user)
 
-    def create(self, request):
-        serializer = self.get_serializer(data=request.data)
-        serializer.is_valid(raise_exception=True)
-        res = self.perform_create(serializer)
-        headers = self.get_success_headers(serializer.data)
-        if res:
-            serializer.data["balance"] = str(self.request.user.balance)
-            return Response((serializer.data), headers=headers)
-        else:
-            return Response({"message": "Insuffitient funds for operation.", "balance": str(self.request.user.balance)})
+    def get(self, request):
+        operations = OperationsSerializer(Operations.objects.all(), many=True)
+        return Response(operations.data)
 
-    # def perform_create(self, serializer):
-    #     amount = Decimal(self.request.data['amount'])
-    #     balance_record = Balance.objects.filter(user=self.request.user).first()
-    #     if not balance_record and amount > 0:
-    #         balance_record = Balance.objects.create(user=self.request.user, balance=amount)
-    #     else:
-    #         if amount < 0 and balance_record.balance < abs(amount):
-    #             return 
-    #         balance_record.balance += amount
-    #         balance_record.save()
-    #     serializer.save(user=self.request.user,
-    #                     amount=self.request.data['amount'],
-    #                     category=self.request.data['category'],
-    #                     organization=self.request.data['organization'])
-    #     return 1
-
-    def perform_create(self, serializer):
-        amount = Decimal(self.request.data['amount'])
-        balance_record = Balance.objects.filter(user=self.request.user).first()
-        if not balance_record and amount > 0:
-            balance_record = self.balance_serializer.save(user=self.request.user, balance=amount)
-        else:
-            if amount < 0 and balance_record.balance < abs(amount):
-                return 
-            balance_record.balance += amount
-            balance_record.save()
-        return serializer.save(user=self.request.user,
-                        amount=self.request.data['amount'],
-                        rest_balance=balance_record.balance,
-                        category=self.request.data['category'],
-                        organization=self.request.data['organization'])
-        # print(self.request.data['amount'])
-        # return serializer.save(data=self.request.data)
-
+    def post(self, request):
+        amount = Decimal(request.data['amount'])
+        operation = OperationsSerializer(data=request.data)
+        if operation.is_valid():
+            balance_record = Balance.objects.filter(user=self.request.user).first()
+            if not balance_record and amount > 0:
+                balance_record = self.balance_serializer.save(user=self.request.user, balance=amount)
+            else:
+                if amount < 0 and balance_record.balance < abs(amount):
+                    return Response({"message": "Insuffitient funds for operation.", "balance": str(self.request.user.balance)})
+                balance_record.balance += amount
+                balance_record.save()
+                operation.save(user=self.request.user,
+                            rest_balance=balance_record.balance)
+                return Response(operation.data)
+        return Response(operation.errors, status=status.HTTP_400_BAD_REQUEST)
